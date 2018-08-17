ad0985dd951d9270810b5b2ad75c70fe97bc1f6e
Vladimir Gorshunov <gorshunov.vladimir@gmail.com>
2018-08-17 10:13:51 +0200

    Add ping-pong table

---
+++ src/components/Form.vue
@@ -0,0 +1,115 @@
+<template>
+  <form autocomplete="off" @submit.prevent="handleSubmit">
+    <label for="email">Email</label>
+    <input id="email" name="email" type="text" v-model="user.email" required>
+
+    <label for="name">Full name</label>
+    <input id="name" name="name" type="text" v-model="user.name" required>
+
+    <label for="password">Password</label>
+    <input id="password" name="password" type="password" v-model="user.password" required>
+    <button type="submit" :disabled="isLoading">
+      {{isLoading ? ' ' : 'Enter'}}
+    </button>
+  </form>
+</template>
+
+<script>
+  export default {
+    name: 'Form',
+    props: {
+      'user': Object,
+      'isLoading': Boolean,
+      'handleSubmit': Function
+    }
+  }
+</script>
+
+<style scoped>
+  p, input, button {
+    color: #303030;
+    width: 24.5rem;
+    font-size: 1.0625rem;
+    font-family: Georgia, serif;
+    line-height: 1.455;
+    letter-spacing: 0.1px;
+    word-spacing: -0.25px;
+  }
+
+  p a {
+    color: #303030;
+    text-decoration: underline;
+  }
+
+  .wf-active p,
+  .wf-active input,
+  .wf-active button {
+    font-family: 'pt-serif', serif;
+  }
+
+  form {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+
+    display: flex;
+    flex-direction: column;
+    width: 432px;
+    margin: 0 auto;
+  }
+
+  label {
+    margin-bottom: 8px;
+  }
+
+  input {
+    margin-bottom: 16px;
+    line-height: 40px;
+    font-size: 16px;
+    padding: 0 16px;
+    outline: none;
+    box-shadow: none;
+    background-color: #fff;
+    color: #303030;
+    border: 1px solid #575757;
+    width: 100%;
+  }
+
+  input:-webkit-autofill,
+  input:-webkit-autofill:hover,
+  input:-webkit-autofill:focus,
+  textarea:-webkit-autofill,
+  textarea:-webkit-autofill:hover,
+  textarea:-webkit-autofill:focus,
+  select:-webkit-autofill,
+  select:-webkit-autofill:hover,
+  select:-webkit-autofill:focus {
+    -webkit-text-fill-color: #303030;
+  }
+
+  input:focus {
+    outline: none;
+    border: 1px solid rgba(48, 48, 48, 0.9);
+  }
+
+  button {
+    border: 0;
+    line-height: 40px;
+    min-height: 43px;
+    display: inline-block;
+    color: #fff;
+    max-width: 168px;
+    cursor: pointer;
+    background-color: rgba(48, 48, 48, 1);
+  }
+
+  button:disabled, button:disabled:hover {
+    background-color: #a2a2a2;
+    cursor: default;
+  }
+
+  button:hover {
+    background-color: #575757;
+  }
+</style>
--- src/components/PingPong.vue
+++ src/components/PingPong.vue
@@ -2,28 +2,38 @@
   <div>
     <navigation class="nav--404"></navigation>
     <div class="ping-pong">
-      <form autocomplete="off" @submit.prevent="handleSubmit">
-        <label for="email">Email</label>
-        <input id="email" name="email" type="text" v-model="user.email">
+      <login-form :user.sync="user" :handleSubmit="handleSubmit" :isLoading="isLoading" v-if="loggedIn"/>
+      <div class="table-wrapper" v-else>
+        <div class="table">
+          <div class="table-head">
+            <div v-for="player in players">
+              <span>{{player.name}}</span>
+            </div>
+          </div>
+          <div class="table-left">
+            <div v-for="player in players">{{player.name}}</div>
+          </div>
+          <div class="table-content">
+            <div class="table-row" v-for="player in players">
+              <div v-for="(subplayer, index) in players">
+                <span v-if="table && table[player.id] && table[player.id][subplayer.id]">{{table[player.id][subplayer.id].home + ':' + table[player.id][subplayer.id].away}}</span>
+                <span v-else>0:0</span>
+                <!--<span>{{index + ' : ' + index}}</span>-->
+              </div>
+            </div>
+          </div>
+        </div>
 
-        <label for="name">Full name</label>
-        <input id="name" name="name" type="text" v-model="user.name">
-
-        <label for="password">Password</label>
-        <input id="password" name="password" type="password" v-model="user.password">
-        <button type="submit">Enter</button>
-      </form>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
   import Navigation from './Navigation'
+  import Form from './Form'
   import firebase from 'firebase/app'
-  import * as firebaseui from 'firebaseui'
-
-  console.log('firebase ', firebase)
-  console.log('firebaseui ', firebaseui)
+  // import uuidv4 from 'uuid/v4'
 
   const config = {
     apiKey: 'AIzaSyCQoJQe1fQK3zOb-pM0-nuhKpywyzd4P-E',
@@ -38,11 +48,34 @@
   export default {
     name: 'about',
     props: [],
-    components: { navigation: Navigation },
+    components: { navigation: Navigation, loginForm: Form },
     data () {
       return {
         isLoading: false,
-        isLoaded: false,
+        loggedIn: false,
+        players: [
+          {id: 1, name: 'Vladimir'},
+          {id: 2, name: 'Ludvig'},
+          {id: 3, name: 'Ludwig'},
+          {id: 4, name: 'David'},
+          {id: 5, name: 'Vladimir'},
+          {id: 6, name: 'Ludvig'},
+          {id: 7, name: 'Ludwig'},
+          {id: 8, name: 'David'},
+          {id: 9, name: 'Vladimir'},
+          {id: 10, name: 'Ludvig'},
+          {id: 11, name: 'Ludwig'},
+          {id: 12, name: 'David'}
+        ],
+        matches: [],
+        table: {
+          1: {
+            2: {
+              home: 1,
+              away: 2
+            }
+          }
+        },
         user: {
           email: '',
           name: '',
@@ -65,20 +98,21 @@
             console.log(error)
             if (error) {
               this.createUser(email, password)
-            } else {
-              this.isLoading = false
             }
+            this.isLoading = false
           })
-          .then(data => {
+          .then(() => {
             firebase.auth().currentUser.updateProfile({
               displayName: this.user.name
             }).then(() => {
+              console.log('firebase.auth().currentUser ', firebase.auth().currentUser)
+
+              this.isLoading = false
+              this.loggedIn = true
               // Update successful.
             }).catch(error => {
               console.log('error ', error)
             })
-
-            console.log('firebase.auth().currentUser ', firebase.auth().currentUser)
           })
       },
       createUser (email, password) {
@@ -100,11 +134,11 @@
             }).then(() => {
               // Update successful.
               this.isLoading = false
+
+              console.log('firebase.auth().currentUser ', firebase.auth().currentUser)
             }).catch(error => {
               console.log('error ', error)
             })
-
-            console.log('firebase.auth().currentUser ', firebase.auth().currentUser)
           })
       },
       handleSubmit (e) {
@@ -116,92 +150,125 @@
         console.log('password ', password)
 
         this.signIn(email, password)
+
+        /*
+  table {
+    display: flex;
+    flex-direction: column;
+  }
+
+  thead {
+    display: flex;
+    justify-content: space-evenly;
+  }
+
+  tbody {
+    display: flex;
+    flex-direction: column;
+  }
+  tr {
+    display: flex;
+    justify-content: space-evenly;
+    width: 100%;
+  }
+
+  tr th {
+    box-sizing: border-box;
+    min-height: 36px;
+    display: inline-block;
+    padding: 8px 0;
+    font-size: 16px;
+    line-height: 24px;
+    flex-grow: 1;
+  }
+
+  tr td {
+    box-sizing: border-box;
+    min-height: 36px;
+    display: inline-block;
+    padding: 8px 0;
+    font-size: 16px;
+    line-height: 24px;
+    flex-grow: 1;
+    text-align: center;
+  }
+
+  @for $i from 1 through 16 {
+    tr:nth-child(#{$i}) td:nth-child(#{$i + 1}) {
+      background-color: #a2a2a2;
+      color: #a2a2a2;
+    }
+  }
+  tr th:first-child,
+  tr td:first-child {
+    padding-left: 0;
+    text-align: left;
+    width: 100px;
+    font-weight: 700;
+    flex-grow: 1.1;
+  }
+*/
       }
     }
   }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-  p, input, button {
-    color: #303030;
-    width: 24.5rem;
-    font-size: 1.0625rem;
-    font-family: Georgia, serif;
-    line-height: 1.455;
-    letter-spacing: 0.1px;
-    word-spacing: -0.25px;
-  }
+<style scoped lang="scss">
 
-  p a {
-    color: #303030;
-    text-decoration: underline;
-  }
-
-  .wf-active p,
-  .wf-active input,
-  .wf-active button {
-    font-family: 'pt-serif', serif;
-  }
-
-  form {
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-
-    display: flex;
-    flex-direction: column;
-    width: 432px;
+  .table-wrapper {
+    padding: 1.5rem 4.5rem;
     margin: 0 auto;
+    font-size: 1rem;
   }
 
-  label {
-    margin-bottom: 8px;
+  .table {
+    display: grid;
+    grid-template-columns: 120px 1fr;
+    grid-template-rows: 120px 1fr;
   }
 
-  input {
-    margin-bottom: 16px;
-    line-height: 40px;
-    font-size: 16px;
-    padding: 0 16px;
-    outline: none;
-    box-shadow: none;
-    background-color: #fff;
-    color: #303030;
-    border: 1px solid #575757;
-    width: 100%;
+  .table-head {
+    grid-column: 2 / 3;
+    grid-row: 1 / 2;
+    display: grid;
+    grid-template-columns: repeat(12, 1fr);
+    grid-template-rows: 120px;
+    align-items: center;
+    & > * {
+      text-align: center;
+    }
+    span {
+      transform: rotate(-90deg);
+      display: inline-block;
+    }
   }
 
-  input:-webkit-autofill,
-  input:-webkit-autofill:hover,
-  input:-webkit-autofill:focus
-  textarea:-webkit-autofill,
-  textarea:-webkit-autofill:hover
-  textarea:-webkit-autofill:focus,
-  select:-webkit-autofill,
-  select:-webkit-autofill:hover,
-  select:-webkit-autofill:focus {
-    -webkit-text-fill-color: #303030;
+  .table-left {
+    grid-column: 1 / 2;
+    grid-row: 2 / 3;
+    display: grid;
+    grid-template-rows: repeat(12, 40px);
+    align-items: center;
+    & > * {
+      text-align: left;
+    }
   }
 
-  input:focus {
-    outline: none;
-    border: 1px solid rgba(48, 48, 48, 0.9);
+  .table-content {
+    grid-column: 2 / 3;
+    grid-row: 2 / 3;
+    display: grid;
+    grid-template-rows: repeat(12, 40px);
+    align-items: center;
   }
-
-  button {
-    border: 0;
-    line-height: 40px;
-    display: inline-block;
-    color: #fff;
-    max-width: 168px;
-    cursor: pointer;
-    background-color: rgba(48, 48, 48, 1);
+  .table-row {
+    display: grid;
+    grid-template-columns: repeat(12, 1fr);
+    grid-template-rows: 40px;
+    align-items: center;
+    & > * {
+      text-align: center;
+    }
   }
-
-  button:hover {
-    background-color: #575757;
-  }
-
 </style>
